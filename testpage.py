b1b5ad9a138ada6f668454cd48eef8679ab99a62
ZGong1 <43224451+ZGong1@users.noreply.github.com>
2019-09-09 19:59:42 -0500

    security updates

--- testpage.py
+++ testpage.py
@@ -1,5 +1,4 @@
 from flask import *
-import sqlite3 as sql
 import os
 import time
 
@@ -8,26 +7,50 @@ app = Flask(__name__)
 #loads db on the main page
 @app.route('/')
 def initial_redirect():
-	return render_template('index.html')
+	return note_list()
 
+@app.route('/change/<file>', methods = ['POST', 'GET'])
+def change_file(file):
+	if session['secret'] == 'ZGong':
+		new = request.form['new']
+		os.system(f"mv /root/Flask/templates/notes/\"{file}\" /root/Flask/templates/backups")
+		with open(f"/root/Flask/templates/notes/{file}", 'w') as f:
+			f.write(new)
+	return redirect('/')
+
+@app.route('/edit/<file>')
+def edit_file(file):
+	if session['secret'] == 'ZGong':
+		with open(f"/root/Flask/templates/notes/{file}", 'r') as f:
+			old = f.read()
+		return render_template('edit.html', file = file, old = old)
+	return redirect('/')
+
+@app.route('/addpage')
+def add_page():
+	if session['secret'] == 'Secret code':
+		return render_template('index.html')
+	return redirect('/')
 
 @app.route('/addnotes', methods = ['POST', 'GET'])
 def add_notes():
-	notes = request.form['notes']
-	date = time.strftime("%m-%d-%Y-%H-%M-%S")
-	subject = request.form['subject']
-	with open(f"./templates/notes/{subject}:{date}.txt", "a") as f:
-		f.write(notes)
-	return initial_redirect()
+	if session['secret'] == 'Secret code':
+		notes = request.form['notes']
+		date = time.strftime("%m-%d-%Y-%H-%M-%S")
+		subject = request.form['subject']
+		with open(f"./templates/notes/{subject}:{date}.txt", "a") as f:
+			f.write(notes)
+		return initial_redirect()
+	return redirect('/')
 
 @app.route('/note_list')
 def note_list():
 	filelist = []
 	for file in os.listdir('./templates/notes'):
 		filelist.append(file)
+	filelist.sort()
 	return render_template('list.html', files = filelist)
 
-#update with its own template and not just spit out a return
 @app.route('/open/<file>')
 def open_file(file):
 	files = []
@@ -41,8 +64,16 @@ def open_class():
 	for file in os.listdir('./templates/notes'):
 		if subject in file:
 			files.append(file)
+	files.sort()
 	return render_template('subject.html', files = files)
+
+@app.route('/addsecret', methods = ['POST', 'GET'])
+def add_secret():
+	secret = request.form['secret']
+	session['secret'] = secret
+	return redirect('/')
 
 #sets up the flask app
 if __name__ == '__main__':
+#insert secret key to keep the session safe    app.config['SECRET_KEY'] = ''
     app.run(host="0.0.0.0", port=25110)
